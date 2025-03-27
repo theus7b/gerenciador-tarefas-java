ed5cd627a49da1fc80d471e21a06c86babf1c81c
theus7b <matheusuvitor.mm@gmail.com>
2025-03-26 21:29:03 -0300

    [update] main updated

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,10 @@
 package main;
 
+import view.TaskView;
+
 public class Main {
+    public static void main(String[] args) {
+        TaskView taskView = new TaskView();
+        taskView.showMenu();
+    }
 }
